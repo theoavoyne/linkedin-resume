f1ae70b7d59b3916c7d375c59b8e8b3379381fe0
Theo Avoyne <tavoyne@gmail.com>
2018-04-28 19:34:08 -0300

    initialized oauth linkedin for development only

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:linkedin]
+
+  def self.find_for_linkedin_oauth(auth)
+    user_params = auth.to_h.slice("provider", "uid")
+    user_params.merge! auth.info.slice(:first_name, :last_name, :headline, :email)
+    user_params[:location_name] = auth.extra.raw_info.location.name
+    user_params[:location_country] = auth.extra.raw_info.to_h["location"]["country"].first[1]
+    user_params[:linkedin_profile_url] = auth.info.urls.public_profile
+    user_params[:linkedin_picture_url] = auth.info.image
+
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user ||= User.where(email: auth.info.email).first
+
+    if user
+      user.update(user_params)
+    else
+      user = User.new(user_params)
+      user.password = Devise.friendly_token[0, 20]
+      user.save
+    end
+
+    return user
+  end
+
 end
--- db/migrate/20180424161213_add_details_to_users.rb
+++ db/migrate/20180424161213_add_details_to_users.rb
@@ -3,12 +3,9 @@ class AddDetailsToUsers < ActiveRecord::Migration[5.1]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :linkedin_profile_url, :string
-    add_column :users, :summary, :text
     add_column :users, :location_name, :string
     add_column :users, :location_country, :string
     add_column :users, :headline, :text
     add_column :users, :linkedin_picture_url, :string
-
-    rename_column :users, :email, :email_address
   end
 end
