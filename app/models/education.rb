ea4a4506594e39bdc757221f3bbcc5b9ac6d59c4
Olivier Riccini <olivier-webdev@hotmail.com>
2018-04-29 17:42:18 -0400

    valiations done

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,3 +1,9 @@
 class Education < ApplicationRecord
   belongs_to :user
+
+  validates :school_name, presence: true
+  validates :field_of_study, presence: true
+  validates :degree, presence: true
+  validates :start_date, presence: true
+  # For :end_date, it depends if :is_current
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,17 @@
 class Position < ApplicationRecord
   belongs_to :user
+
+  validates :company_name, presence: true
+  validates :summary, presence: true
+
+  validates :summary, length: {
+    maximum: 2000, message:
+    "should be less than 2000 characters"
+    # Linkedin validation
+  }
+
+  validates :start_date, presence: true
+  # For :end_date, it depends if :is_current
+
+  validates :title, presence: true
 end
