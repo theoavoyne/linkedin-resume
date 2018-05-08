3cf2d4385907aa8d4db74996c0903b5cebafceea
Theo Avoyne <tavoyne@gmail.com>
2018-05-08 15:11:52 -0400

    correcting error with initializers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,11 @@ class PagesController < ApplicationController
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: "editor"   # Excluding ".pdf" extension.
+        render pdf: "editor",
+        template: "pages/editor.html.erb",
+        layout: 'pdf.html',
+        show_as_html: params.key?('debug'),
+        margin: { top: 20 }
       end
     end
   end
