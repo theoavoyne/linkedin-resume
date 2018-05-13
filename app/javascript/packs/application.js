34d4cf3908e0e6d45dda2fb9a5169dad0fe817c2
Olivier Riccini <olivier-webdev@hotmail.com>
2018-05-13 12:31:45 -0400

    still working on design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
 import "bootstrap";
+
+$('#scroll-to-pick-a-template').click(function(){
+  let heightToScroll = $('.flex-banner').height() + $('.navbar-custom').height();
+  console.log(heightToScroll);
+  $('html, body').animate({scrollTop : heightToScroll});
+  return false;
+});
