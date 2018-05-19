bffa381c5d7018533dfa0ba3883f218652440eb3
Theo Avoyne <tavoyne@gmail.com>
2018-05-18 20:51:56 -0500

    changed unuseful files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,11 +11,11 @@ class PagesController < ApplicationController
   end
 
   def resume
-    render pdf: "#{current_user.first_name}-#{current_user.last_name}-resume",
-    template: "templates/_#{current_user.template}.html.erb", #see this file in the views
+    render pdf: "#{current_user.first_name.downcase}-#{current_user.last_name.downcase}-resume",
+    template: "templates/#{current_user.template}.html.erb", #see this file in the views
     layout: 'pdf.html',
     show_as_html: params.key?('debug'),
     margin: { top: 20, bottom: 28 },
-    footer: { html: { template: "templates/_#{current_user.template}_footer.html.erb", layout: 'footer.html' } }
+    footer: { html: { template: "templates/#{current_user.template}_footer.html.erb", layout: 'footer.html' } }
   end
 end
