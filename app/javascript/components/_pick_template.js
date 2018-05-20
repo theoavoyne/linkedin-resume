4df2dcd13446d8de9d957ca7971670781f07da2a
Olivier Riccini <olivier-webdev@hotmail.com>
2018-05-20 11:17:38 -0400

    tweets embed

--- app/javascript/components/_pick_template.js
+++ app/javascript/components/_pick_template.js
@@ -4,17 +4,10 @@ const step2 = document.querySelector('.middle-rectangle span');
 const viewMyResumeBtn = document.querySelector(".btn-to-editor");
 
 viewMyResumeBtn.disabled = true;
-// if ( viewMyResumeBtn.disabled ) {
-//   viewMyResumeBtn.classList.add('btn-inactive');
-// } else {
-//   viewMyResumeBtn.classList.remove('btn-inactive');
-// }
-
 
 function pickTemplate(templateName) {
   pickTemplateInput.value = templateName;
   viewMyResumeBtn.disabled = false;
-  // viewMyResumeBtn.classList.remove('btn-inactive');
   step2.innerHTML = `<i class="fas fa-check"></i>`
   templates.forEach(function(template) {
     template.classList.remove('template-selected');
